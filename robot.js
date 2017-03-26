2a63f45b5700bc2a6e8e71b7f6c4f69567d7ecab
Ryan Furness <ryanfurness@northwesternmutual.com>
2017-03-26 16:05:54 -0500

    initial commit

---
+++ robot.js
@@ -0,0 +1,17 @@
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({
+    debug: false
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+        token: ''
+}).startRTM();
+
+// give the bot something to listen for.
+controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
+
+    bot.reply(message,'Hello yourself.');
+
+});
